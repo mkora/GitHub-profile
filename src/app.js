e1ec58fec9bd6e6d564a9e968f9947a8d0720fca
mkor <31717889+mkora@users.noreply.github.com>
2018-01-25 15:36:46 -0500

    Added api calls to access user data

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('config');
 
-const octokit = require('@octokit/rest')();
+const github = require('@octokit/rest')();
 
 const app = express();
 app.use(bodyParser.urlencoded({
@@ -22,17 +22,81 @@ app.get('/pulse', (req, res) => {
   });
 });
 
-app.get('/api/user/:username', async (req, res) => {
+app.get('/api/user/:username', async (req, res, next) => {
+  const paginate = async (method, params = {}) => {
+    let response = await method(Object.assign({ per_page: 100 }, params));
+    let { data } = response;
+    while (github.hasNextPage(response)) {
+      response = await github.getNextPage(response); // eslint-disable-line no-await-in-loop
+      data = data.concat(response.data);
+    }
+    return data;
+  };
+
+  const { username } = req.params;
+  const token = config.get('GITHUB_ACCESS_TOKEN');
+  if (token === '#REQUIRED') {
+    return next(new Error('GitHub access token is missing. Please, update a config file'));
+  }
+
   try {
-    const { username } = req.params;
-    const { data } = await octokit.users.getForUser({ username });
-    return res.json({
-      ok: true,
-      data
+    await github.authenticate({
+      type: 'oauth',
+      token: config.get('GITHUB_ACCESS_TOKEN'),
     });
+
+    const { meta, data } = await github.users.getForUser({ username });
+
+    const repos = await paginate(github.repos.getForUser, { username });
+
+    try {
+      const repoStats = await Promise.all(repos.map(async (repo) => {
+        const langs = await github.repos.getLanguages({
+          owner: username,
+          repo: repo.name,
+          per_page: 100
+        });
+        return {
+          name: repo.name,
+          description: repo.description,
+          stars: repo.stargazers_count,
+          languages: langs.data
+        };
+      }));
+
+      return res.json({
+        ok: true,
+        ratelimit: {
+          limit: meta['x-ratelimit-limit'],
+          remaining: meta['x-ratelimit-remaining'],
+        },
+        user: {
+          login: data.login,
+          name: data.name,
+          github_url: data.url,
+          avatar_url: data.avatar_url,
+          since: data.created_at,
+          company: data.company,
+          location: data.location,
+          repos_number: data.public_repos
+        },
+        repos: repoStats
+      });
+    } catch (err) {
+      res.status(err.code);
+      return res.json({
+        message: err.message
+      });
+    }
   } catch (err) {
     res.status(err.code);
-    return res.json(JSON.parse(err.message));
+    let message;
+    try {
+      message = JSON.parse(err.message);
+    } catch (e) {
+      message = { message: err.message };
+    }
+    return res.json(message);
   }
 });
 
