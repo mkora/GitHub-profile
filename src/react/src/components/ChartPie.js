e662df5c65c664c0eaa47827189701fb816f9a2d
mkor <31717889+mkora@users.noreply.github.com>
2018-02-12 20:16:28 -0500

    Displaying a message if data in pie chart is zero

--- src/react/src/components/ChartPie.js
+++ src/react/src/components/ChartPie.js
@@ -10,49 +10,53 @@ const ChartPie = (props) => {
     );
   }
 
-  // @todo check if all data is 0. nivo doesn't draw 0 values pie
+  const isZero = data.every(v => v.value === 0);
+
   return (
     <div>
       <div>{title}</div>
-      <div style={{ height: 400 }}>
-        <ResponsivePie
-          data={data}
-          margin={{
-            top: 40,
-            right: 80,
-            bottom: 80,
-            left: 80
-          }}
-          innerRadius={0.5}
-          padAngle={1}
-          cornerRadius={3}
-          colors="nivo"
-          colorBy="id"
-          borderColor="inherit:darker(0.6)"
-          radialLabelsSkipAngle={10}
-          radialLabelsTextXOffset={6}
-          radialLabelsTextColor="#333333"
-          radialLabelsLinkOffset={0}
-          radialLabelsLinkDiagonalLength={16}
-          radialLabelsLinkHorizontalLength={24}
-          radialLabelsLinkStrokeWidth={1}
-          radialLabelsLinkColor="inherit"
-          slicesLabelsSkipAngle={10}
-          slicesLabelsTextColor="#333333"
-          animate={true}
-          motionStiffness={90}
-          motionDamping={15}
-          legends={[{
-            anchor: 'bottom',
-            direction: 'row',
-            translateY: 56,
-            itemWidth: 100,
-            itemHeight: 14,
-            symbolSize: 14,
-            symbolShape: 'circle'
-          }]}
-        />
-      </div>
+      {isZero && <div>No data found</div>}
+      {!isZero &&
+        <div style={{ height: 400 }}>
+          <ResponsivePie
+            data={data}
+            margin={{
+              top: 40,
+              right: 80,
+              bottom: 80,
+              left: 80
+            }}
+            innerRadius={0.5}
+            padAngle={1}
+            cornerRadius={3}
+            colors="nivo"
+            colorBy="id"
+            borderColor="inherit:darker(0.6)"
+            radialLabelsSkipAngle={10}
+            radialLabelsTextXOffset={6}
+            radialLabelsTextColor="#333333"
+            radialLabelsLinkOffset={0}
+            radialLabelsLinkDiagonalLength={16}
+            radialLabelsLinkHorizontalLength={24}
+            radialLabelsLinkStrokeWidth={1}
+            radialLabelsLinkColor="inherit"
+            slicesLabelsSkipAngle={10}
+            slicesLabelsTextColor="#333333"
+            animate={true}
+            motionStiffness={90}
+            motionDamping={15}
+            legends={[{
+              anchor: 'bottom',
+              direction: 'row',
+              translateY: 56,
+              itemWidth: 100,
+              itemHeight: 14,
+              symbolSize: 14,
+              symbolShape: 'circle'
+            }]}
+          />
+        </div>
+      }
     </div>
   );
 };
