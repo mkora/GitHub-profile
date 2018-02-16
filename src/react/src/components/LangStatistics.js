579620801ef5f1f5a34aa34c167be3ad640dd4d0
mkor <31717889+mkora@users.noreply.github.com>
2018-02-16 15:07:51 -0500

    Grid for pie charts added

--- src/react/src/components/LangStatistics.js
+++ src/react/src/components/LangStatistics.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import { withStyles } from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
 
+import PropTypes from 'prop-types';
 import ChartPie from './ChartPie';
-import { repoPiedData } from '../utils/chartDataFilters';
+import { langPiedData } from '../utils/chartDataFilters';
 
 const styles = theme => ({
   paper: {
@@ -15,23 +15,31 @@ const styles = theme => ({
   },
 });
 
-const RepoStatistics = (props) => {
+const LangStatistics = (props) => {
   const { classes, data } = props;
   return (
     <Grid container spacing={16}>
-      <Grid item xs={12} sm={6}>
+      <Grid item xs={12} sm={4}>
         <Paper className={classes.paper}>
           <ChartPie
-            title="Commits per Repo"
-            data={repoPiedData(data, 'commit')}
+            title="Bytes per Language"
+            data={langPiedData(data, 'byte')}
           />
         </Paper>
       </Grid>
-      <Grid item xs={12} sm={6}>
+      <Grid item xs={12} sm={4}>
         <Paper className={classes.paper}>
           <ChartPie
-            title="Stars per Repo"
-            data={repoPiedData(data, 'star')}
+            title="Repos per Language"
+            data={langPiedData(data, 'repo')}
+          />
+        </Paper>
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <Paper className={classes.paper}>
+          <ChartPie
+            title="Commits per Language"
+            data={langPiedData(data, 'commit')}
           />
         </Paper>
       </Grid>
@@ -39,9 +47,9 @@ const RepoStatistics = (props) => {
   );
 };
 
-RepoStatistics.propTypes = {
+LangStatistics.propTypes = {
   classes: PropTypes.object.isRequired,
   data: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(RepoStatistics);
+export default withStyles(styles)(LangStatistics);
