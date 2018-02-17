50e4ca06dc75a53dcddb78bce63d6d349929d25c
mkor <31717889+mkora@users.noreply.github.com>
2018-02-17 15:48:33 -0500

    Added styled components to user profile

--- src/react/src/components/UserInfo.js
+++ src/react/src/components/UserInfo.js
@@ -1,36 +1,109 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import moment from 'moment';
+
+import { withStyles } from 'material-ui/styles';
+
+import { Card, CardHeader, CardContent } from 'material-ui';
+
+import Avatar from 'material-ui/Avatar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+
+import BusinessIcon from 'material-ui-icons/Business';
+import EventNoteIcon from 'material-ui-icons/EventNote';
+import LocationOnIcon from 'material-ui-icons/LocationOn';
+import StorageIcon from 'material-ui-icons/Storage';
+import GitHubIcon from '../icons/GitHubIcon';
+
+const styles = theme => ({
+  avatar: {
+    width: 100,
+    height: 100,
+  },
+  icon: {
+    marginRight: theme.spacing.unit,
+
+  },
+});
+
 const UserInfo = (props) => {
-  const isPlaced = props.company || props.location;
+  const { classes } = props;
+
   return (
-    <div>
-      <div style={{ float: 'left', width: '50%' }}>
-        <img
-          src={props.avatarUrl}
-          alt={`Profile of ${props.username}`}
-          height="150"
-          width="150"
-        />
-      </div>
-      <div style={{ float: 'right', width: '50%' }}>
-        <p>{props.username} {props.realname ? `(${props.realname})` : ''}</p>
-        <p><a href={props.profileUrl}>GitHub</a></p>
-        { isPlaced &&
-          <p>
-            {props.company ? `Works at '${props.company}'` : ''}
-            {props.company && props.location ? ',' : ''}
-            {props.location ? props.location : ''}
-          </p>
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar
+            alt={`Profile of ${props.username}`}
+            src={props.avatarUrl}
+            className={classes.avatar}
+          />
         }
-        <p>Member Since: {props.memberSince}</p>
-        <p>Public Repos: {props.reposNumber}</p>
-      </div>
-    </div>
+        title={props.username}
+        subheader={props.realname}
+        action={
+          <IconButton
+            className={classes.icon}
+            aria-label="View prifile on GitHub"
+            target="_blank"
+            href={props.profileUrl}
+          >
+            <GitHubIcon
+              color="action"
+              titleAccess="GitHub profile"
+            />
+          </IconButton>
+        }
+      />
+      <CardContent>
+        <Typography paragraph>
+          <EventNoteIcon
+            className={classes.icon}
+            color="primary"
+            titleAccess="Member Since"
+          />
+          Joined {moment(props.memberSince).format('dddd, MMMM Do YYYY')}
+        </Typography>
+
+        <Typography paragraph>
+          <StorageIcon
+            className={classes.icon}
+            color="primary"
+            titleAccess="Public Repos"
+          />
+          Created {props.reposNumber} repos
+        </Typography>
+
+        { props.company &&
+          <Typography paragraph>
+            <BusinessIcon
+              className={classes.icon}
+              color="primary"
+              titleAccess="Works at"
+            />
+            {props.company}
+          </Typography>
+        }
+
+        { props.location &&
+          <Typography paragraph>
+            <LocationOnIcon
+              className={classes.icon}
+              color="primary"
+              titleAccess="Location"
+            />
+            {props.location}
+          </Typography>
+        }
+      </CardContent>
+    </Card>
   );
 };
 
 UserInfo.propTypes = {
+  classes: PropTypes.object.isRequired,
   username: PropTypes.string.isRequired,
   realname: PropTypes.string,
   profileUrl: PropTypes.string.isRequired,
@@ -48,4 +121,4 @@ UserInfo.defaultProps = {
   location: '',
 };
 
-export default UserInfo;
+export default withStyles(styles)(UserInfo);
