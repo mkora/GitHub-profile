a765ae2fa75f19b79f656554d25adfa75dec6625
mkor <31717889+mkora@users.noreply.github.com>
2018-03-03 19:38:05 -0500

    Usage of client side local storage removed

--- src/react/src/api/github.js
+++ src/react/src/api/github.js
@@ -1,18 +1,9 @@
 import axios from 'axios';
-import { hit, miss } from '../utils/cache';
 
 export default async function githab(username) {
   try {
-    let data = hit(username);
-    if (data !== null) {
-      return data;
-    }
-    data = await axios.get(`/api/user/${username}`);
-    const result = miss(username, data.data);
-    if (result !== false) {
-      return Promise.resolve(data.data);
-    }
-    return Promise.reject(data.data);
+    const data = await axios.get(`/api/user/${username}`);
+    return Promise.resolve(data.data);
   } catch (error) {
     return Promise.reject(error.response);
   }
--- src/react/src/config/index.js
+++
@@ -1,4 +0,0 @@
-export default {
-  localDataLifetime: 86400000, // 24 hours
-  localStorageKeyPrefix: 'profile',
-};
